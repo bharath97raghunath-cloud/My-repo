58dd9361dec6a34633762812deaf19cf2f9bf5e7
bharath97raghunath-cloud <bharath97.raghunath@gmail.com>
2025-10-30 00:18:33 -0700

    first commit

---
+++ src/main/java/Impl/ImplApplication.java
@@ -0,0 +1,15 @@
+package Impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplApplication.class, args);
+        System.out.println("HI");
+	}
+    
+
+}
